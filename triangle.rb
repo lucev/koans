7bb187e6b0dc3bb11ffcbb67338eb996866c5f84
Ivan Lučev <ivan.lucev@refactorit.co>
2016-04-28 18:14:46 +0200

    About triangle project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if a == b && b == c
+  return :isosceles if a - b == 0 || b - c == 0 || c - a == 0 
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
